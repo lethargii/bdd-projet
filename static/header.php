1654b48c3c1db396bee4e05c89ab7aedd1f0d630
Myafeux <myafeux@gmail.com>
2025-05-18 16:00:55 +0200

    peut-être que ca va résoudre un pb?

--- static/header.php
+++ static/header.php
@@ -4,6 +4,6 @@
             <p>(Je promet il est vrm bien)</p>
         </div>
         <a href="index.html">
-            <img src="images/imagesSite/logoGrand.png" alt="logoGrand" width="200">
+            <img src="../images/imagesSite/logoGrand.png" alt="logoGrand" width="200">
         </a>
 </header>
