7874dfa744281782a3f7d3314f7eaf1d26f0b5ef
lethargii <l.sevault@hotmail.com>
2025-05-18 20:14:57 +0200

    Début search8

--- creaJeu/index.php
+++ creaJeu/index.php
@@ -35,7 +35,7 @@ include("../static/head.php");
           <input type="number" id="prix" name="prix" placeholder="Prix à la sortie" required>
           <input type="date" id="dateSortie" name="dateSortie" placeholder="Date de sortie" required>
           <input type="text" id="synopsis" name="synopsis" placeholder="Synopsis" required>
-          <input type="file" id="imageJeu" name="imageJeu" accept="image/png, image/jpeg" />
+          <input type="file" id="imageJeu" name="imageJeu" accept="image/png, image/jpeg" required>
           <?php
             $categories = listCategorie($mysqli);
             $supports = listSupport($mysqli);
